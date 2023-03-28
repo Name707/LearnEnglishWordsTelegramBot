2bedcd9923f346a766abd7357df3dc1b09993eae
Name707 <707VLAN@gmail.com>
2023-03-28 08:30:10 +0600

    englishLearnBot-stage3 Исправлено замечание - "повтор кода"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,15 +14,17 @@ fun main() {
         dictionary.add(Word(lineOfWordsList[0], lineOfWordsList[1], lineOfWordsList[2].toInt()))
 
     }
+    val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }.size
+
     while (true) {
         println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
 
         when (readln().toInt()) {
             1 -> println("Выбрали команду 1")
             2 -> println(
-                "Список содержит ${dictionary.filter { it.correctAnswersCount >= 3 }.size} выученных слов." +
-                        "\nВыучено ${dictionary.filter { it.correctAnswersCount >= 3 }.size} из ${dictionary.size} слов | " +
-                        "${(((dictionary.filter { it.correctAnswersCount >= 3 }.size).toDouble() / dictionary.size) * 100).roundToInt() }%"
+                "Список содержит $learnedWords выученных слов." +
+                        "\nВыучено $learnedWords из ${dictionary.size} слов | " +
+                        "${(((learnedWords).toDouble() / dictionary.size) * 100).roundToInt()}%"
             )
 
             0 -> break

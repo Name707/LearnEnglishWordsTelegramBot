5c919688ec28c2354af7dc8c93e99247fe13f9a0
Name707 <707VLAN@gmail.com>
2023-03-30 12:29:13 +0600

    englishLearnBot-stage5 Реализован функционал: 1. Считывать ответ пользователя и сравнивать с правильным ответом по индексу списка слов для вопросов.
    2. Создать функцию сохранения обновленных результатов в словарь и вызвать ее при каждом правильном ответе.
    
    3. В вариантах ввода добавить новый пункт (0): Меню (направляет пользователя в главное меню).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,11 @@ fun main() {
     val dictionary: MutableList<Word> = mutableListOf()
     val wordsToLinesList: List<String> = wordsFile.readLines()
 
+    fun saveDictionary() {
+        wordsFile.writeText("")
+        for (word in dictionary) wordsFile.appendText(("${word.original}|${word.translate}|${word.correctAnswersCount}\n"))
+    }
+
     for (line in wordsToLinesList) {
         val lineOfWordsList = line.split("|")
         dictionary.add(Word(lineOfWordsList[0], lineOfWordsList[1], lineOfWordsList[2].toInt()))
@@ -23,17 +28,23 @@ fun main() {
             1 -> do {
                 val unlearnedWordsList = dictionary.filter { it.correctAnswersCount < 3 }
                 val randomFourUnlearnedWords = unlearnedWordsList.shuffled().take(4)
+                val randomWordForLearn = randomFourUnlearnedWords.randomOrNull()
 
                 if (unlearnedWordsList.isEmpty()) {
                     println("Вы выучили все слова")
                     break
                 } else {
-                    println("Выберите правильный перевод слова \"${randomFourUnlearnedWords.random().original}\":\n" +
+                    println("Введите цифру правильного варианта перевода слова \"${randomWordForLearn?.original}\". Цифра \"0\" для выхода в меню:\n" +
                             randomFourUnlearnedWords.map { it.translate }
                     )
-                    val userAnswer = readln()
-                }
+                    val userAnswer = readln().toInt()
 
+                    if (userAnswer != 0 && (randomFourUnlearnedWords[userAnswer - 1].translate == randomWordForLearn?.translate)) {
+                        dictionary.map { if (it.translate == randomWordForLearn.translate) it.correctAnswersCount++ }
+                        saveDictionary()
+                    } else if (userAnswer == 0) break
+
+                }
             } while (unlearnedWordsList.isNotEmpty())
 
             2 -> println(
@@ -51,7 +62,7 @@ fun main() {
 class Word(
     val original: String,
     val translate: String,
-    val correctAnswersCount: Int = 0,
+    var correctAnswersCount: Int = 0,
 ) {
 
 }
